a2958e9abf3694d959df2430edc76e33eb235b3b
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-03-01 22:31:51 +0100

    upload

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
+const header = <h1 className='title'>Witaj na mojej stronie!</h1>
+const main = div>h1+p
 
 
 
-class App extends React.Component {
-  state = {
-    counter: 0
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hello!</h1>
-      </div>
-    );
-  }
-}
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+
+
+// const element5 = (<div>
+//   <section></section>
+//   <section></section>
+// </div>)
+
+
+// class App extends React.Component {
+//   state = {
+//     counter: 0
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello!</h1>
+//       </div>
+//     );
+//   }
+// }
+
+
+// ReactDOM.render(<App />, document.getElementById('root'))
